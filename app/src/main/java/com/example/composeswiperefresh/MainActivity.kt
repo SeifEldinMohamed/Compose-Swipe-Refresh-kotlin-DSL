74818b1931c9b7c6cac72cbe3f0e365b505b8ab8
Seif Mohamed <seifeldinmohamed101@gmail.com>
2023-04-05 18:14:34 +0200

    swipe refresh added

--- app/src/main/java/com/example/composeswiperefresh/MainActivity.kt
+++ app/src/main/java/com/example/composeswiperefresh/MainActivity.kt
@@ -1,18 +1,27 @@
 package com.example.composeswiperefresh
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.material.pullrefresh.PullRefreshIndicator
+import androidx.compose.material.pullrefresh.pullRefresh
+import androidx.compose.material.pullrefresh.rememberPullRefreshState
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.composeswiperefresh.ui.theme.ComposeSwipeRefreshTheme
 
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterialApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -22,7 +31,28 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    //val viewModel: MainViewModel = hiltViewModel()
+                    var isRefreshing by remember { // in viewmodel
+                        mutableStateOf<Boolean>(false)
+                    }
+
+                    val pullRefreshState = rememberPullRefreshState(isRefreshing, {
+                        Log.d("news", "pulled")
+                        // make call again from viewmodel
+                        isRefreshing = false
+                    })
+
+                    Box(
+                        Modifier
+                            .pullRefresh(pullRefreshState)
+                            .verticalScroll(rememberScrollState())
+                    ) {
+                        PullRefreshIndicator(
+                            refreshing = isRefreshing,
+                            state = pullRefreshState,
+                            modifier = Modifier.align(Alignment.TopCenter)
+                        )
+                    }
                 }
             }
         }
